2b37025d3a6d79048a61e8d58875058b7f6aea29
StevOrc <steven.orcun2@gmail.com>
2021-01-17 11:15:22 +0100

    add age on appComp

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 function App() {
   const name = "Steven";
+  const age = 34;
   return (
     <div className="App">
       <header className="App-header">
         Welcome back {name} !
+        you are {34} years old !
       </header>
     </div>
   );
